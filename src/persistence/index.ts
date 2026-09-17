e0dea9575ac2b9f711c22529297b62947795ae2f
Nikhilesh R Sahu <153189774+NikhileshRSahu@users.noreply.github.com>
2026-09-17 22:45:59 +0530

    feat: export provider connection schema

--- src/persistence/index.ts
+++ src/persistence/index.ts
@@ -1,4 +1,5 @@
 export * from './database.js';
+export * from './provider-connections-schema.js';
 export * from './repositories/index.js';
 export * from './schema.js';
 export * from './tenant.js';
