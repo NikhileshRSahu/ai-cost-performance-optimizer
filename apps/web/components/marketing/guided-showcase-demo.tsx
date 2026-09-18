740650739151ec2cb660bbc3d556ed7bc11a88c2
Nikhilesh R Sahu <153189774+NikhileshRSahu@users.noreply.github.com>
2026-09-19 01:55:27 +0530

    chore: recommit Launch stack preview v2

--- apps/web/components/marketing/guided-showcase-demo.tsx
+++ apps/web/components/marketing/guided-showcase-demo.tsx
@@ -51,7 +51,7 @@ function VerifyScreen() {
   </div>
 }
 
-// Recommit: Launch-style stacked showcase deployment trigger\nexport function GuidedShowcaseDemo(){
+// Recommit: Launch-style stacked showcase deployment trigger v2\nexport function GuidedShowcaseDemo(){
   const ref=useRef<HTMLElement>(null);
   const {scrollYProgress}=useScroll({target:ref,offset:['start start','end end']});
   const x1=useTransform(scrollYProgress,[0,1],['-18%','-34%']);
